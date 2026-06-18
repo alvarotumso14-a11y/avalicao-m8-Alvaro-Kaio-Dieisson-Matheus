b02ac381dff4d4144f803e3f08bfa2460f8e3072
kablumenschein <ka.blumenschein@hotmail.com>
2026-06-18 20:42:12 -0300

    add IRepositorioLivro

---
+++ avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Interfaces/IRepositorioLivro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Interfaces
+{
+    internal interface IRepositorioLivro
+    {
+    }
+}
