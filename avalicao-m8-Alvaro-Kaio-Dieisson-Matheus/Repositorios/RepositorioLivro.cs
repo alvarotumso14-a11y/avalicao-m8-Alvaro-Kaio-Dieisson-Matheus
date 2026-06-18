66b8c763fb1cf7eefc219f4e13e2b7011e5fc25a
dieisson <dieisson.qfloriano@gmail.com>
2026-06-18 20:59:51 -0300

    adicionado os metodos em repositorio

--- avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Repositorios/RepositorioLivro.cs
+++ avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Repositorios/RepositorioLivro.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 
 namespace avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Repositorios
 {
+        {
+        private List<Livros> Livros = new();
 
-  
+    public void Adicionar(Livros L)
+    {
+        Livros.add(L);
+    }
+
+    public List<Livros> ListarTodos()
+    {
+        return Livros;
+    }
+
+    public List<Livros> BuscarPorAutor(string autor)
+    {
+        return Livros
+            .Where(p => p.Autor == autor)
+            .ToList();
+    }
+
+    public Livro BuscarPorId(int id)
+    {
+        var livro = Livros.FirstOrDefault(p => p.Id == id);
+        return livro ?? throw new LivroNaoEncontradoExceptio($"Id {id} nao encontrado");
+    }
+
+
 }
+
