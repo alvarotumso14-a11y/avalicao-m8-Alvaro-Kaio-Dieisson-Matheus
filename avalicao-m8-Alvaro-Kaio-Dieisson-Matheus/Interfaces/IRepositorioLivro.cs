0da00dd5c7465a8b3d991a12d3949e5a11377e32
dieisson <dieisson.qfloriano@gmail.com>
2026-06-18 20:32:16 -0300

    add a interface de RepositorioLivro

---
+++ avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Interfaces/IRepositorioLivro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Interfaces
+{
+    public interface IRepositorioLivro
+    {
+        {
+        void Adicionar(Livro L);
+
+        List<Livro> ListarTodos();
+
+        List<Livro> BuscarPorAutor(string autor);
+
+        public Livro BuscarPorId(int id);
+    }
+
+}
+}
