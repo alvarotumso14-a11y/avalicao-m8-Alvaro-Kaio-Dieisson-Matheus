8857f94aacd6732f91252524a0edcb9f300539a0
kablumenschein <ka.blumenschein@hotmail.com>
2026-06-18 20:57:51 -0300

    add RepositorioLivro

---
+++ avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Repositorios/RepositorioLivro.cs
@@ -0,0 +1,12 @@
+﻿using avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Repositorios
+{
+
+  
+}
