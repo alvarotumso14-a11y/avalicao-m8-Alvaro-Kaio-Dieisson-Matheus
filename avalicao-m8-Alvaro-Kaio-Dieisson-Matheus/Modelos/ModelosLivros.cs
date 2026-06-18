f54f421b2d7a9c91ae4f219bf83c54333f642e96
Álvaro Ribeiro <alvaro63466506@alunos.sc.senac.br>
2026-06-18 20:27:31 -0300

    Modelos Livros

---
+++ avalicao-m8-Alvaro-Kaio-Dieisson-Matheus/Modelos/ModelosLivros.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avalicao_m8_Alvaro_Kaio_Dieisson_Matheus.Modelos
+{
+    class ModelosLivros
+    {
+
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public int Ano { get; set; }
+
+        public bool Disponivel { get; set; }
+
+
+        public Livro(int id, string titulo, string autor, int ano, bool disponivel)
+
+        {
+
+            Id = id;
+
+            Titulo = titulo;
+
+            Autor = autor;
+
+            Ano = ano;
+
+            Disponivel = disponivel;
+
+        }
+
+
+    }
+
+}
+}
